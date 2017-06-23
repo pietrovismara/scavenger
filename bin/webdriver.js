bdeb23552a3c7da522204229c77f3de762c2c912
Andrea <gorgonauta84@yahoo.it>
2017-06-23 15:19:54 +0200

    fix missing default useragent

--- bin/webdriver.js
+++ bin/webdriver.js
@@ -188,6 +188,7 @@ function load(url, debugMode) {
 
 function setUserAgent(useragent) {
     debug('setting useragent: ' + useragent);
+    useragent = useragent || "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
     return nightmare.get().useragent(useragent);
 }
 
